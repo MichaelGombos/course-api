e1376c40f26798ec1743f72235f89fc33c035173
MichaelGombos <gombos2002@gmail.com>
2020-12-23 21:04:11 -0600

    Update Topic Request mapping
    Changed request mapping of Topic Controller to return a list instead of string

--- src/main/java/io/javabrains/springbootstarter/topic/TopicController.java
+++ src/main/java/io/javabrains/springbootstarter/topic/TopicController.java
@@ -1,5 +1,8 @@
 package io.javabrains.springbootstarter.topic;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class TopicController {
 
 	@RequestMapping("/topics")
-	public String getAllTopics() {
-		return "All Topics";
+	public List<Topic> getAllTopics() {
+		return Arrays.asList(
+				new Topic("spring","the spring framework","spring framework description"),
+				new Topic("java","core java","Core java Description"),
+				new Topic("JSON","JSON for dummies","JSON for dummies description")
+				);
 	}
 }
