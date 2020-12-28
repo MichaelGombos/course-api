cae0d3bc71122f4d85a555c97f8737e5eececf74
MichaelGombos <gombos2002@gmail.com>
2020-12-28 16:19:01 -0600

    Added Course APIs
    Added a course api layer to our repository. allowing us to search for a specific course, UNDER a topic.

---
+++ src/main/java/io/javabrains/springbootstarter/course/Course.java
@@ -0,0 +1,64 @@
+package io.javabrains.springbootstarter.course;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import io.javabrains.springbootstarter.topic.Topic;
+
+@Entity
+public class Course {
+
+	@Id
+	private String id;
+	private String name;
+	private String description;
+	
+	private Topic topic;
+	
+	public Course() {
+		
+	}
+	
+	public Course(String id, String name, String description, String topicId) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.topic = new Topic(topicId,"",""); //emp name and desc new instance of topic 
+	}
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	/**
+	 * @return the topic
+	 */
+	public Topic getTopic() {
+		return topic;
+	}
+
+	/**
+	 * @param topic the topic to set
+	 */
+	public void setTopic(Topic topic) {
+		this.topic = topic;
+	}
+	
+	
+}
